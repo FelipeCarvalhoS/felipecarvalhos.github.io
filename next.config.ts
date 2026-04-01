c268023198ba842531f7e99ca87708fc7e797c37
Felipe de Carvalho Santos <142360153+FelipeCarvalhoS@users.noreply.github.com>
2026-04-01 20:07:25 -0300

    Chore: set up sass and react-bootstrap

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-    /* config options here */
     reactStrictMode: true,
+    sassOptions: {
+        implementation: 'sass',
+    },
 }
 
 export default nextConfig
