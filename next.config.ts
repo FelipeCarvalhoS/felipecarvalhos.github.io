d2f5e5b713102265b4443947f17682dd12174d5c
FelipeCarvalhoS <142360153+FelipeCarvalhoS@users.noreply.github.com>
2026-05-26 21:07:03 -0300

    Chore: enable React Compiler

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
+    reactCompiler: true,
     reactStrictMode: true,
     sassOptions: {
         implementation: 'sass',
